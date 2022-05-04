43d085fadac81102bbb43868144e03cc24fd9239
VitoriaL22 <vitorialp00@gmail.com>
2022-05-04 08:37:42 -0300

    criei tela contato

---
+++ Telas/Contato.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function Contato() {
+    return (
+      <View>
+        <Button>aaaaaa</Button>
+        <br/>
+        <Button>aaaaa</Button>
+        <br/>
+        
+        <Text>alalalalla</Text>
+      </View>
+    );
+  }
+
+  export default Contato;
